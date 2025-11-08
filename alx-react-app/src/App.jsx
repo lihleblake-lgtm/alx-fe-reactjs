257cca6b198da86131df426af34fdb207c00e871
Thubelihle Bianca <lihleblake@gmail.com>
2025-11-08 14:29:19 -0800

    added a new file UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,15 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
+import React from "react";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <h1>User Profile</h1>
+      <UserProfile
+      name="Alice"
+      age="25"
+      bio="Loves hiking and photography"
+      />
     </div>
   );
 }
