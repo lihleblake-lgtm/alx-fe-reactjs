0e70168aed34480b9861cf0b576f9f913e37a700
Thubelihle Bianca <lihleblake@gmail.com>
2025-11-08 21:52:54 -0800

    edited maincontent file

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,6 +4,8 @@ import UserProfile from './UserProfile';
 function MainContent() {
     return (
         <main style={{ backgroundColor: '#e8f0fe', padding: '20px', minHeight: '60vh' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+            
             <UserProfile name="Luzuko" age="3" bio="Abright and loving soul who brings joy everywhere she goes." />
             <UserProfile name="Bianca" age="22" bio="Creative visionary." />
         </main>
