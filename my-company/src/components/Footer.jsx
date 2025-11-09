7c6eaeca66728cd54ceee986ae87f7cce763e87e
Thubelihle Bianca <lihleblake@gmail.com>
2025-11-09 01:31:20 -0800

    added my-company

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#004466',
+      color: 'white',
+      textAlign: 'center',
+      padding: '10px',
+      marginTop: '20px'
+    }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
