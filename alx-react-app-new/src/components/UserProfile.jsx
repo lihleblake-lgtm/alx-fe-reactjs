fd7f779faf8aae0d7000686407bcf2db6c8922df
Thubelihle Bianca <lihleblake@gmail.com>
2025-11-08 21:42:27 -0800

    added 10px on userprofile

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function UserProfile(props) {
     return (
-        <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
             <h2 style={{ color: 'blue', marginBottom: '8px' }}>{props.name}</h2>
             <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
