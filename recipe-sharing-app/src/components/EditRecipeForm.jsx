d1203c964ceb1d31752948f2d06ea3d2f68a6e41
Thubelihle Bianca <lihleblake@gmail.com>
2025-11-16 22:09:23 -0800

    edited editrecipeforms.jsx

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -4,8 +4,8 @@ const EditRecipeForm = ({ recipe }) => {
   const updateRecipe = useRecipeStore((state) => state.updateRecipe);
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     updateRecipe({
       id: recipe.id,
       title,
