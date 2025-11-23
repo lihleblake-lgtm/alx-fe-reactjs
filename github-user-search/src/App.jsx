70668eedd431c357d1703ec5b452baf65a925d5f
Thubelihle Bianca <lihleblake@gmail.com>
2025-11-23 14:06:38 -0800

    created github-user-research

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Github User Search</h1>
+      <p>Your setup is complete. Ready to build!</p>
+    </div>
+  );
+}
+
+export default App;
